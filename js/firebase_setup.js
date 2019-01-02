2e86e16bd4a7b467001f564ec83cd4fd220ba8cb
damon ren <demiao.ren@gmail.com>
2019-01-02 12:22:42 -0800

    firebase setup credential

---
+++ js/firebase_setup.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDzH2HrkYSUU0pt_u6T0_31HN_3ewuYRQM",
+    authDomain: "co-op-sep2018-hackthon-team07.firebaseapp.com",
+    databaseURL: "https://co-op-sep2018-hackthon-team07.firebaseio.com",
+    projectId: "co-op-sep2018-hackthon-team07",
+    storageBucket: "co-op-sep2018-hackthon-team07.appspot.com",
+    messagingSenderId: "527884200678"
+  };
+  firebase.initializeApp(config);

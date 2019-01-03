2758c020495dd6e94e3544000caf8f3734268399
damon ren <demiao.ren@gmail.com>
2019-01-02 16:31:59 -0800

    updated table headers

--- public/js/find.js
+++ public/js/find.js
@@ -16,6 +16,8 @@ $(function() {
         groupLocation: snapshot.child('location').val(),
         groupMax: snapshot.child('max').val(),
         groupType: snapshot.child('type').val(),
+        groupTime: snapshot.child('time').val(),
+        groupDate: snapshot.child('date').val(),
       };
       addRow(groupInfo);
     });
@@ -29,10 +31,6 @@ $(function() {
     td_name.text(info.groupName);
     newRow.append(td_name);
 
-    var td_email = $('<td></td>');
-    td_email.text(info.groupEmail);
-    newRow.append(td_email);
-
     var td_location = $('<td></td>');
     td_location.text(info.groupLocation);
     newRow.append(td_location);
@@ -44,6 +42,18 @@ $(function() {
     var td_max = $('<td></td>');
     td_max.text(info.groupMax);
     newRow.append(td_max);
+
+    var td_time = $('<td></td>');
+    td_time.text(info.groupTime);
+    newRow.append(td_time);
+
+    var td_date = $('<td></td>');
+    td_date.text(info.groupDate);
+    newRow.append(td_date);
+
+    // var td_email = $('<td></td>');
+    // td_email.text(info.groupEmail);
+    // newRow.append(td_email);
 
     var td_button = $('<td></td>');
     td_button.html('<button>Request</button>');

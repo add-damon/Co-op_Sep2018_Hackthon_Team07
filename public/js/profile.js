b4d50229e873df91f7cabd031af93606c82db37e
Vincent Wong <vwong125@my.bcit.ca>
2019-01-02 22:15:09 -0800

    worked on profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -6,15 +6,15 @@ let profileFirebaseRef = firebase.database().ref("users/" + uid);
 
 //Display current profile
 profileFirebaseRef.on(
-    "value",   
-    function(snap) {
+    "value",
+    function (snap) {
         //console.log(JSON.stringify(snap.val()));
 
-        $("#user-name").val(snap.val().name);
-        $("#email").val(snap.val().email);
-        $("#phone").val(snap.val().phone);
-        $("#description").val(snap.val().description);
-});
+        // $("#user-name").val(snap.val().name);
+        // $("#email").val(snap.val().email);
+        // $("#phone").val(snap.val().phone);
+        // $("#description").val(snap.val().description);
+    });
 
 function generateProfileInformation() {
     // Create the object to hold all the information
@@ -29,20 +29,70 @@ function generateProfileInformation() {
     formInfoObj.name = userName;
     formInfoObj.phone = phone;
     formInfoObj.description = description;
-    
+
     return formInfoObj;
 }
 
 //Firebase database updating
-$("#submit-button").on("click", function(e) {
+$("#submit-button").on("click", function (e) {
     let profileInfo = generateProfileInformation();
 
-    profileFirebaseRef.update(profileInfo, function(error) {
+    profileFirebaseRef.update(profileInfo, function (error) {
         if (error) {
             alert('Something wrong happend. Try again!');
         } else {
             alert('You have successfully updated your profile!');
         }
-    }).then(function(){
+    }).then(function () {
     });
 });
+
+// Get Current User Information
+
+let email = userInfo.email;
+let numberMatched = 0
+let groupName;
+let yourGroups = document.getElementById('your-groups');
+
+// checks if the database contains any groups with the email = the current user
+firebase.database().ref('groups/').on('value', function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
+        let userEmail = childSnapshot.val().email;
+        console.log(userEmail);
+        if (userEmail === email) {
+            console.log('yo');
+            yourGroups.innerHTML = '';
+            numberMatched += 1;
+            groupName = childSnapshot.key;
+        }
+    })
+})
+
+// Create table elements if numberMatched is greater than 0
+setTimeout(function () {
+    console.log('hi')
+    console.log(numberMatched)
+    for (let i = 0; i < numberMatched; i++) {
+        let row = yourGroups.insertRow(i);
+        row.id = 'row' + i;
+        console.log(row.id);
+
+        let cell1 = row.insertCell(0);
+        cell1.innerHTML = groupName;
+
+        let cell2 = row.insertCell(1);
+        let anchor = document.createElement('a');
+        anchor.innerHTML = 'edit';
+        anchor.href = 'https://www.google.com/';
+        cell2.appendChild(anchor);
+
+        let cell3 = row.insertCell(2);
+        let delbutton = document.createElement('button');
+        delbutton.innerHTML = 'del';
+        cell3.appendChild(delbutton);
+
+    }
+
+}, 1000);
+
+
